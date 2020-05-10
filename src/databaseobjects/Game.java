6cb64c108339c8cddeafbbbd4370f8e10823273d
Cullen LaKemper <lakempca@rose-hulman.edu>
2020-05-10 02:25:38 -0500

    started gmae connection service

--- src/databaseobjects/Game.java
+++ src/databaseobjects/Game.java
@@ -1,5 +1,27 @@
 package databaseobjects;
 
+import java.awt.Image;
+import java.sql.Date;
+
 public class Game {
 
+	String name;
+	Date dateMade;
+	String consoles;
+	Image image;
+	
+	public Game(String name, Date dateMade, String consoles, Image image){
+		this.name = name;
+		this.dateMade = dateMade;
+		this.consoles = consoles;
+		this.image = image;
+	}
+	public Game(String name, Date dateMade, String consoles){
+		this.name = name;
+		this.dateMade = dateMade;
+		this.consoles = consoles;
+	}
+	
+	
+	
 }
