cc4f81c8b9ffc0dc966ee1e42baca4e226627be4
Cullen LaKemper <lakempca@rose-hulman.edu>
2020-05-18 20:42:50 -0500

    changed login bypass string

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -224,7 +224,7 @@ public class MainWindow {
 	//To turn this off, simply comment out the method in the Main Class.
 	public void bypassLogin() {
 		dbConnection.connect(Reader.getAttribute("serverName"), Reader.getAttribute("databaseName"), Reader.getAttribute("defaultUsername"), Reader.getAttribute("defaultPassword"));
-		connect("LOGIN BYPASSED");
+		connect(Reader.getAttribute("defaultUsername"));
 	}
 
 }
