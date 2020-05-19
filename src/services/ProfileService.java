8c6c68f5a2a094368d9637b1475885d58b712176
stapler <stapler@rose-hulman.edu>
2020-05-18 23:29:52 -0400

    favorite game and character can be seen from profile screen

--- src/services/ProfileService.java
+++ src/services/ProfileService.java
@@ -1,5 +1,11 @@
 package services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import databaseobjects.SelectedItem;
+
 public class ProfileService {
 	
 	DatabaseConnection dbConnection;
@@ -7,6 +13,37 @@ public class ProfileService {
 	public ProfileService(DatabaseConnection dbConnection) {
 		this.dbConnection = dbConnection;
 	}
-
+	
+	public String getFavGame(String username) {
+		 try {
+			Statement fgST = dbConnection.getConnection().createStatement();
+			String fgQuery = "SELECT Game FROM [User Favorite] WHERE Username = "+"'"+username+"'";
+			ResultSet fgRS = fgST.executeQuery(fgQuery);
+			
+			int gameIndex = fgRS.findColumn("Game");
+			
+			fgRS.next();
+			return fgRS.getString(gameIndex);
+			
+		} catch (SQLException e) {
+		}
+		 return "No favorite game";
+	}
+	
+	public String getFavChar(String username) {
+		 try {
+			Statement fcST = dbConnection.getConnection().createStatement();
+			String fcQuery = "SELECT Character FROM [User Favorite] WHERE Username = "+"'"+username+"'";
+			ResultSet fcRS = fcST.executeQuery(fcQuery);
+			
+			int charIndex = fcRS.findColumn("Character");
+			
+			fcRS.next();
+			return fcRS.getString(charIndex);
+			
+		} catch (SQLException e) {
+		}
+		return "No favorite character";
+	}
 
 }
--- src/ui/ProfilePanel.java
+++ src/ui/ProfilePanel.java
@@ -1,22 +1,50 @@
 package ui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+
 import services.ProfileService;
 
 public class ProfilePanel extends ViewPanel {
 	
 	ProfileService profileService; 
  	String username;
+	JLabel user;
+	JLabel fGame;
+	JLabel fChar;
+	
 
 	public ProfilePanel(ProfileService profileService) {
 		super();
 		this.remove(game);
 		activeViewName.setText("Profile");
 		this.profileService = profileService;
+		
 	}
+	
+	public void draw(){
+		this.add(user);
+		user.setBounds(250, 50, 1000, 30);
+		user.setForeground(Color.WHITE);
+		
+		this.add(fGame);
+		fGame.setBounds(250, 150, 1000, 30);
+		fGame.setForeground(Color.WHITE);
+		this.add(fChar);
+		fChar.setBounds(250, 250, 1000, 30);
+		fChar.setForeground(Color.WHITE);
+	}
+	
 	
 	public void setConnected(String username) {
 		this.add(activeViewName);
 		this.username = username;
+		user = new JLabel("User: " + username);
+		fGame = new JLabel("Favorite Game: " + profileService.getFavGame(username));
+		fChar = new JLabel("Favorite Character: " + profileService.getFavChar(username));
+		this.draw();
 		this.revalidate();
 		this.repaint();
 	}
@@ -27,5 +55,6 @@ public class ProfilePanel extends ViewPanel {
 		this.revalidate();
 		this.repaint();
 	}
+	
 	
 }
