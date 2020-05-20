465fb25c1a5f53ff6bbd3cd18c2eb58bf8873c92
stapler <stapler@rose-hulman.edu>
2020-05-19 22:25:56 -0400

    favorites can be edited from profile

--- src/services/ProfileService.java
+++ src/services/ProfileService.java
@@ -1,8 +1,12 @@
 package services;
 
+import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
+
+import javax.swing.JOptionPane;
 
 import databaseobjects.SelectedItem;
 
@@ -44,6 +48,80 @@ public class ProfileService {
 		} catch (SQLException e) {
 		}
 		return "No favorite character";
+	}
+	
+	public int getRank(String username) {
+		 try {
+			Statement rST = dbConnection.getConnection().createStatement();
+			String rQuery = "SELECT Rank FROM [User] WHERE Username = "+"'"+username+"'";
+			ResultSet rRS = rST.executeQuery(rQuery);
+			
+			int rankIndex = rRS.findColumn("Rank");
+			
+			rRS.next();
+			return rRS.getInt(rankIndex);
+			
+		} catch (SQLException e) {
+		}
+		 return 0;
+	}
+	
+	public boolean updateFav(String username, String game, String character) {
+		try {
+			if (username.equals("appUserSuperSmash")) {
+				JOptionPane.showMessageDialog(null, "Cannot edit favorites when login is bypassed");
+				return false;
+			}
+			CallableStatement str = dbConnection.getConnection().prepareCall("{? = call dbo.update_Fav(?,?,?)}");
+			str.registerOutParameter(1, Types.INTEGER);
+			str.setNString(2, username);
+			str.setNString(3, game);
+			str.setNString(4, character);
+			str.executeUpdate();
+			int returnValue = str.getInt(1);
+
+			if (returnValue == 0) {
+				JOptionPane.showMessageDialog(null, "Favorites were successfully updated");
+				return true;
+			}
+
+			if (returnValue == 1) {
+				JOptionPane.showMessageDialog(null, "ERROR: Game currently does not exist in the Database");
+			}
+			
+			if (returnValue == 2) {
+				JOptionPane.showMessageDialog(null, "ERROR: Character currently does not exist in the Database.");
+			}
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Was unsuccessful in updating favorites");
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean deleteProfile(String username) {
+		if (username.equals("appUserSuperSmash")) {
+			JOptionPane.showMessageDialog(null, "Cannot delete profile when login is bypassed");
+			return false;
+		}
+		try {
+			CallableStatement str = dbConnection.getConnection().prepareCall("{? = call dbo.delete_User(?)}");
+			str.registerOutParameter(1, Types.INTEGER);
+			str.setNString(2, username);
+			str.executeUpdate();
+			int returnValue = str.getInt(1);
+
+			if (returnValue == 0) {
+				JOptionPane.showMessageDialog(null, "Profile deleted");
+				return true;
+			}
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Was unsuccessful in deleting profile");
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
--- src/ui/ProfilePanel.java
+++ src/ui/ProfilePanel.java
@@ -2,18 +2,35 @@ package ui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import services.ProfileService;
 
 public class ProfilePanel extends ViewPanel {
 	
+	MainWindow window;
 	ProfileService profileService; 
  	String username;
 	JLabel user;
+	JLabel rank;
 	JLabel fGame;
 	JLabel fChar;
+	JLabel eGame;
+	JLabel eChar;
+	JButton edit;
+	JButton delete;
+	JButton save;
+	JButton discard;
+	JTextField editGame;
+	JTextField editChar;
+	
+	
 	
 
 	public ProfilePanel(ProfileService profileService) {
@@ -25,25 +42,120 @@ public class ProfilePanel extends ViewPanel {
 	}
 	
 	public void draw(){
+		user = new JLabel("User: " + username);
 		this.add(user);
 		user.setBounds(250, 50, 1000, 30);
 		user.setForeground(Color.WHITE);
 		
+		rank = new JLabel("Rank: " + profileService.getRank(username));
+		this.add(rank);
+		rank.setBounds(250, 150, 1000, 30);
+		rank.setForeground(Color.WHITE);
+		
+		fGame = new JLabel("Favorite Game: " + profileService.getFavGame(username));
 		this.add(fGame);
-		fGame.setBounds(250, 150, 1000, 30);
+		fGame.setBounds(250, 250, 1000, 30);
 		fGame.setForeground(Color.WHITE);
+		
+		fChar = new JLabel("Favorite Character: " + profileService.getFavChar(username));
 		this.add(fChar);
-		fChar.setBounds(250, 250, 1000, 30);
+		fChar.setBounds(250, 350, 1000, 30);
 		fChar.setForeground(Color.WHITE);
+		
+		edit = new JButton("Edit Favorites");
+		this.add(edit);
+		edit.setBounds(250, 425, 200, 30);
+		edit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				edit();
+			}});
+		
+		delete = new JButton("Delete Profile");
+		this.add(delete);
+		delete.setBounds(500, 425, 200, 30);
+		delete.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					window.disconnect();
+					profileService.deleteProfile(username);
+			}});
+		
+		eGame = new JLabel("Enter favorite game");
+		eGame.setBounds(250, 240, 200, 30);
+		eGame.setForeground(Color.WHITE);
+		
+		eChar = new JLabel("Enter favorite character");
+		eChar.setBounds(250, 340, 200, 30);
+		eChar.setForeground(Color.WHITE);
+		
+		editGame = new JTextField();
+		editGame.setBounds(250, 270, 200, 30);
+		
+		editChar = new JTextField();
+		editChar.setBounds(250, 370, 200, 30);
+		
+		save = new JButton("Save Changes");
+		save.setBounds(500, 425, 200, 30);
+		save.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				profileService.updateFav(username, editGame.getText(), editChar.getText());
+				updateFavs();
+			}});
+		
+		discard = new JButton("Discard Changes");
+		discard.setBounds(250, 425, 200, 30);
+		discard.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				discard();
+			}});
+	}
+	
+	public void edit() {
+		this.remove(fGame);
+		this.remove(fChar);
+		this.remove(edit);
+		this.remove(delete);
+		this.add(editGame);
+		this.add(editChar);
+		this.add(eGame);
+		this.add(eChar);
+		this.add(save);
+		this.add(discard);
+		this.repaint();
+	}
+	
+	public void discard() {
+		this.add(fGame);
+		this.add(fChar);
+		this.add(edit);
+		this.add(delete);
+		this.remove(editGame);
+		this.remove(editChar);
+		this.remove(eGame);
+		this.remove(eChar);
+		this.remove(save);
+		this.remove(discard);
+		this.repaint();
+	}
+	
+	public void updateFavs() {
+		fGame.setText("Favorite Game: " + profileService.getFavGame(username));
+		fChar.setText("Favorite Character: " + profileService.getFavChar(username));
+		discard();
 	}
 	
 	
-	public void setConnected(String username) {
+	public void setConnected(String username, MainWindow window) {
 		this.add(activeViewName);
 		this.username = username;
-		user = new JLabel("User: " + username);
-		fGame = new JLabel("Favorite Game: " + profileService.getFavGame(username));
-		fChar = new JLabel("Favorite Character: " + profileService.getFavChar(username));
+		this.window = window;
 		this.draw();
 		this.revalidate();
 		this.repaint();
@@ -52,6 +164,18 @@ public class ProfilePanel extends ViewPanel {
 	@Override
 	public void setDisconnected() {
 		this.remove(activeViewName);
+		this.remove(user);
+		this.remove(rank);
+		this.remove(fGame);
+		this.remove(fChar);
+		this.remove(edit);
+		this.remove(delete);
+		this.remove(editGame);
+		this.remove(editChar);
+		this.remove(eGame);
+		this.remove(eChar);
+		this.remove(save);
+		this.remove(discard);
 		this.revalidate();
 		this.repaint();
 	}
